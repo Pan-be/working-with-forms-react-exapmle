663c2a1f2cbb6489b9a76fb451163a6038f2c0a8
Konrad <konradbieniek@gmail.com>
2022-12-25 22:20:45 +0100

    handling inputs with useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,28 @@
-const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
-};
+import { useRef, useState } from "react"
 
-export default SimpleInput;
+const SimpleInput = (props) => {
+	const [enteredName, setEnteredName] = useState("")
+
+	const nameInputChangeHandler = (e) => {
+		setEnteredName(e.target.value)
+	}
+
+	const onSubmitHandler = (e) => {
+		e.preventDefault()
+		console.log(enteredName)
+	}
+
+	return (
+		<form onSubmit={onSubmitHandler}>
+			<div className='form-control'>
+				<label htmlFor='name'>Your Name</label>
+				<input type='text' id='name' onChange={nameInputChangeHandler} />
+			</div>
+			<div className='form-actions'>
+				<button>Submit</button>
+			</div>
+		</form>
+	)
+}
+
+export default SimpleInput
